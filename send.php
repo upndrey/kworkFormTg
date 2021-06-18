04d8dd24f947896b35c3f71f7107ae0287ce7e6e
Andrey <upndrey@yandex.ru>
2021-06-18 19:59:31 +0700

    Добавление портфолио, футера и сохранения данных в бд

--- send.php
+++ send.php
@@ -1,19 +1,18 @@
 <?php
+require_once "connection.php";
 if(isset($_POST['submit'])) {
     $name = $_POST['name'];
     $phone = $_POST['phone'];
     $rooms = $_POST['rooms'];
-    // сюда нужно вписать токен вашего бота
     define('TELEGRAM_TOKEN', '1824312426:AAGPjCvZZclAJSuulCHRm8XWaR15ID6slg4');
-    // сюда нужно вписать ваш внутренний айдишник
     define('TELEGRAM_CHATID', '-1001415200128');
-    // Проверяем последнюю цифру кол-ва комнат для корректного окончания
-
     $text = "Новое сообщение: \n" .
     "Имя: " . $name . "\n" .
     "Номер телефона: " . $phone . "\n" .
     "Количество комнат: " . $rooms;
     message_to_telegram($text);
+    $query = "INSERT INTO messages (name, phone, rooms) VALUES ('$name', '$phone', '$rooms')";
+    mysqli_query($link, $query);
 }
 
 function message_to_telegram($text) {
